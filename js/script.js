b16f1551a4ce5310f4af81b701a3a72f9d446405
Giulia <giuliatullia93@gmail.com>
2021-06-21 00:14:50 +0200

    add-milestone-1-2

---
+++ js/script.js
@@ -0,0 +1,144 @@
+// Milestone 1 Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout. 
+// Milestone 2 Coloriamo le icone per tipo
+// Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+const icons = [
+    {
+      nome: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'dragon',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      nome: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      nome: 'apple-alt',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      nome: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      nome: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      nome: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      nome: 'user-graduate',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      nome: 'user-ninja',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      nome: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+  ];
+
+   
+
+const arrayColorato = icons.map((element) => {
+
+  let color = 'pink';
+
+  if(element.type == 'animal'){
+    color = 'blue';
+  }else if(element.type == 'vegetable'){
+    color = 'green';
+  }
+  return{
+    ...element,
+    color
+  } 
+});
+
+print(arrayColorato);
+
+console.log(arrayColorato);
+
+// funzioni
+   function print(array){
+
+    array.forEach((element) => {
+      
+      // destrutturazione
+       const {color, nome, prefix, family} = element;
+  
+       const elementHTML = `
+       <div>
+         <i class="${family} ${prefix}${nome}" style = "color:${color}"></i>
+         <div class="title">${nome.toUpperCase()}</div>
+       </div>
+       `;
+  
+       document.getElementById('icons').innerHTML += elementHTML;
+    
+    });
+  
+    }
